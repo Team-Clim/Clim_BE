a94fde3967ab7a0e24e0afdd41b7d6ca00b0d9e5
troy96475 <taesupark0504@gmail.com>
2025-04-17 14:29:25 +0900

    first commit

---
+++ src/main/java/com/example/clim_be/ClimBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.clim_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clim_be/ClimBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clim_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
