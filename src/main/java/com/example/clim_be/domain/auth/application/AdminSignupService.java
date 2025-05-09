a2d4088bf4cf0bdb88c4d3d5b33230a275831b0a
박태수 <taesupark0504@gmail.com>
2025-05-09 13:39:14 +0900

    feat :: admin 회원가입

---
+++ src/main/java/com/example/clim_be/domain/auth/application/AdminSignupService.java
@@ -0,0 +1,27 @@
+package com.example.clim_be.domain.auth.application;
+
+import com.example.clim_be.domain.admin.domain.Admin;
+import com.example.clim_be.domain.admin.domain.repository.AdminRepository;
+import com.example.clim_be.domain.admin.presentation.dto.request.SignupRequest;
+import com.example.clim_be.domain.auth.presentation.dto.AuthElementDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminSignupService {
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(SignupRequest request) {
+        Admin admin = adminRepository.save(Admin.builder()
+                        .userName(request.getUserName())
+                        .accountId(request.getAccountId())
+                        .password(passwordEncoder.encode(request.getPassword()))
+                        .role(AuthElementDto.Role.ADMIN)
+                .build());
+    }
+}
