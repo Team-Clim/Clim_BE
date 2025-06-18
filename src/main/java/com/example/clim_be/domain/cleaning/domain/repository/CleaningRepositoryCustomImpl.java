6adea9aa962505cc69a79fe4b075c340f22840bd
박태수 <taesupark0504@gmail.com>
2025-06-18 19:20:08 +0900

    feat :: 날짜별 조회 SQL

--- src/main/java/com/example/clim_be/domain/cleaning/domain/repository/CleaningRepositoryCustomImpl.java
+++ src/main/java/com/example/clim_be/domain/cleaning/domain/repository/CleaningRepositoryCustomImpl.java
@@ -1,9 +1,47 @@
 package com.example.clim_be.domain.cleaning.domain.repository;
 
+import com.example.clim_be.domain.cleaning.domain.QCleaning;
+import com.example.clim_be.domain.cleaning.presentation.dto.response.CleaningResponse;
+import com.example.clim_be.domain.user.domain.QUser;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static com.example.clim_be.domain.cleaning.domain.QCleaning.cleaning;
+import static com.example.clim_be.domain.user.domain.QUser.user;
+
 @RequiredArgsConstructor
-public class CleaningRepositoryCustomImpl {
+public class CleaningRepositoryCustomImpl implements CleaningRepositoryCustom{
     private final JPAQueryFactory jpaQueryFactory;
+    private final QCleaning qCleaning = cleaning;
+    private final QUser qUser = user;
+
+    @Override
+    public Optional<CleaningResponse> findCleaningStatusByDate(String userName, LocalDate date) {
+        CleaningResponse response = jpaQueryFactory
+                .select(Projections.constructor(
+                        CleaningResponse.class,
+                        user.userName,
+                        user.roomNumber,
+                        cleaning.date,
+                        cleaning.status,
+                        cleaning.reason
+                ))
+                .from(cleaning)
+                .join(cleaning.user, user)
+                .where(
+                        cleaning.date.eq(date),
+                        user.userName.eq(userName)
+                )
+                .fetchOne();
+
+        return Optional.ofNullable(response);
+    }
+
+
+
+
 }
