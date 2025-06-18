d28db6f309e40a06afa03e7c42772a2950c65f3c
박태수 <taesupark0504@gmail.com>
2025-06-18 19:19:37 +0900

    feat :: 날짜별 조회 쿼리

--- src/main/java/com/example/clim_be/domain/cleaning/domain/repository/CleaningRepositoryCustom.java
+++ src/main/java/com/example/clim_be/domain/cleaning/domain/repository/CleaningRepositoryCustom.java
@@ -1,4 +1,10 @@
 package com.example.clim_be.domain.cleaning.domain.repository;
 
+import com.example.clim_be.domain.cleaning.presentation.dto.response.CleaningResponse;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface CleaningRepositoryCustom {
+    Optional<CleaningResponse> findCleaningStatusByDate(String userName, LocalDate date);
 }
